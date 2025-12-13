4a34d4042d408271325e4fd231b3e1ea21d1aac2
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-13 03:47:36 +0100

    pretty much done with this for now

--- overlay/setoptions.cc
+++ overlay/setoptions.cc
@@ -3,9 +3,12 @@
 void setOptions(int argc, char **argv, 
                 int &size, double &angle, size_t &count)
 {
-    if (argc > 2)
+    if (argc > 1)
     {
         size = stoi(argv[1]);
+    }
+    if (argc > 2)
+    {
         angle = stod(argv[2]);
     }
     if (argc == 4)
